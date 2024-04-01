d789c7855bc5e564d12cf146d13146707131f35b
4leksey8963 <enotggdd@gmail.com>
2024-04-01 15:06:06 +0300

    Second commit: make structure and ended app

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,69 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    static Scanner input;
+    public static void main(String[] args) {
+
+        System.out.println("\n_______________Калькулятор_______________");
+        try
+        {
+            while(true)
+            {
+                // ВВОД ПЕРВОГО ЧИСЛА
+                System.out.print("\nВведите первое число: ");
+                input = new Scanner(System.in);
+                while(!input.hasNextFloat())
+                {
+                    System.out.println("Требуется ввести число!");
+                    input.next();
+                    System.out.print("\nВведите первое число: ");
+                }
+                System.out.println("______________________");
+                float a = input.nextFloat();
+
+                // ВВОД СИМВОЛА ОПЕРАЦИИ
+                System.out.print("\nВведите символ операции (+, -, *, /): ");
+                input = new Scanner(System.in);
+                String operation = input.next();
+                while(!("+-*/".contains(operation) && operation.length() == 1))
+                {
+                    System.out.println("Требуется ввести один из прердложенных символов (+, -, *, /)!");
+                    System.out.print("\nВведите символ операции (+, -, *, /): ");
+                    operation = input.next();
+                }
+                System.out.println("///______________________///");
+
+                // ВВОД ВТОРОГО ЧИСЛА
+                System.out.print("\nВведите второе число: ");
+                input = new Scanner(System.in);
+                while(!input.hasNextFloat())
+                {
+                    input.next();
+                    System.out.print("\nВведите второе число: ");
+                }
+                float b = input.nextFloat();
+
+
+                System.out.println("______________________");
+
+                System.out.println("\n" + a + " " + operation+ " " +  + b + " = " + Calculate.returnCalculate(a,b,operation));
+
+                System.out.println("Для продолжения введите 'a' и нажмите 'Enter'. Чтобы закончить - любая другая клавиша...");
+                input = new Scanner(System.in);
+                if(!((input.next()).toLowerCase()).equals("a")){
+                    break;
+                }
+                System.out.println("______________________");
+            }
         }
+        catch (Exception e)
+        {
+            System.out.print("Ошибка: " + e);
+        }
+
     }
+
 }

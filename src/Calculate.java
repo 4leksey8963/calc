adafcac5d992dc44c58f54b057286f5536f336a3
4leksey8963 <enotggdd@gmail.com>
2024-04-17 10:33:17 +0300

    5 commit: класс Calculate переопределен в интерфейс

--- src/Calculate.java
+++ src/Calculate.java
@@ -1,21 +1,10 @@
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
-public class Calculate {
-    static public ArrayList<String> returnCalculate(int a, int b, String operation)
-    {
-        ArrayList<String> resultArray = new ArrayList<String>();
-        int result = switch (operation) {
-            case "+" -> a + b;
-            case "-" -> a - b;
-            case "/" -> a / b;
-            case "*" -> a * b;
-            default -> 0;
-        };
-        resultArray.add(Integer.toBinaryString(result));
-        resultArray.add(Integer.toOctalString(result));
-        resultArray.add(Integer.toString(result));
-        resultArray.add(Integer.toHexString(result));
-        return resultArray;
-    }
+public interface Calculate {
+
+    public int plus();
+    public int minus();
+    public int multiply();
+    public int division();
 }
